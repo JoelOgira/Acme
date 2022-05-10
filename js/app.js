a6cd80a9c4b2a3f441441c77b5c8a9b7dc26a968
JoelOgira-2020 <joelogira@gmail.com>
2022-05-10 14:10:25 +0300

    Some changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 const counterDispalyArea = document.querySelector('.btn-area');
 const counterDisplay = document.querySelector('.counter-display');
+const counterDisplayContent = document.querySelector('.counter-display-content');
 const closeButton = document.querySelector('.close');
-
+const closeButton2 = document.querySelector('.close-2');
 
 let count = 0;
 counterDispalyArea.addEventListener('click', e => {
-    if (e.target.className === 'close') {
+    if (e.target.className === 'close') {        
         e.target.parentElement.remove();
     }
     if (e.target.id === 'btn-counter') {
@@ -14,8 +15,9 @@ counterDispalyArea.addEventListener('click', e => {
     if (e.target.id === 'btn-subtract') {
         count--;
     }
-    counterDisplay.innerHTML = `
-        <h2 class="pt-3">Counter Display : <span style="color: chocolate;">${count}</span></h2>
+    counterDisplay.style.display = 'block';
+    counterDisplayContent.innerHTML = `
+        <h2 class="pt-1">Counter Display : <span style="color: chocolate; padding-bottom: 12px;">${count}</span></h2>
     `;
 });
 
